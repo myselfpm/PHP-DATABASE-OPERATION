b95d5ba6aff73cf05c8eccfaf9552f3c2322543b
myselfpm <60108573+myselfpm@users.noreply.github.com>
2020-03-13 12:33:38 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>databse connection</title>
+</head>
+<body>
+	<form action="conndb.php" method="POST">
+		<table align="center" >
+			<tr>
+				<td>NAME</td><td><input type="text" name="name" required="required"></td>
+				
+			    
+			</tr>
+			<tr>
+				<td>EMAIL</td><td><input type="text" name="Email" required="required"></td>
+			</tr>
+			<tr >
+				<td colspan="2" align="center">
+
+               <input type="submit" name="submit">
+			    </td>
+			</tr>
+		</table> 
+		
+	</form>
+
+</body>
+</html>
